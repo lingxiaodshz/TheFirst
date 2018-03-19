0a58aca8773ee1fdf53177c35d90f74fc8689510
lingxiaodshz <lingxiaodshz@163.com>
2018-03-19 21:38:10 +0800

    EditText不会自动变大小

--- app/src/main/java/com/lingxiao/thefirst/widget/LastInputActivity.java
+++ app/src/main/java/com/lingxiao/thefirst/widget/LastInputActivity.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.lingxiao.thefirst.R;
 
@@ -17,6 +21,9 @@ import com.lingxiao.thefirst.R;
 
 public class LastInputActivity extends Activity {
     private LastInputEditText mEtInput;
+    private EditText mEtInput1;
+    private TextView mTvMoney;
+    private LinearLayout mLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,9 +31,12 @@ public class LastInputActivity extends Activity {
         setContentView(R.layout.activity_last_input);
 
         mEtInput = findViewById(R.id.et_input);
+        mEtInput1 = findViewById(R.id.et_input1);
+        mTvMoney = findViewById(R.id.tv_money);
+        mLayout = findViewById(R.id.ll_layout);
 
         mEtInput.addTextChangedListener(mInputWatcher);
-
+        mEtInput1.addTextChangedListener(mWatcher);
     }
 
     private TextWatcher mInputWatcher = new TextWatcher() {
@@ -67,6 +77,28 @@ public class LastInputActivity extends Activity {
                 }
             }
             mEtInput.addTextChangedListener(mInputWatcher);
+        }
+    };
+
+    private TextWatcher mWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if (!TextUtils.isEmpty(mEtInput1.getText())) {
+                mTvMoney.setVisibility(View.VISIBLE);
+            } else {
+                mTvMoney.setVisibility(View.GONE);
+            }
+            mEtInput1.measure(View.MeasureSpec.AT_MOST, View.MeasureSpec.AT_MOST);
         }
     };
 }
