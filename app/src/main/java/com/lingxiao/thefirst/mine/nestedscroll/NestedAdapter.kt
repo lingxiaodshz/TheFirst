a5c5866eb82f890be9eaf7cf8084ac5742d03386
lingxiaodshz <lingxiaodshz@163.com>
2018-08-20 16:17:38 +0800

    Kotlin写RecyclerView的Adapter

---
+++ app/src/main/java/com/lingxiao/thefirst/mine/nestedscroll/NestedAdapter.kt
@@ -0,0 +1,33 @@
+package com.lingxiao.thefirst.mine.nestedscroll
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.lingxiao.thefirst.R
+
+class NestedAdapter(context: Context, list: MutableList<String>) : RecyclerView.Adapter<NestedAdapter.NestedHolder>() {
+    private var mContext: Context = context
+    private var mList: MutableList<String> = list
+
+    override fun getItemCount(): Int {
+        if (mList == null) {
+            return 0
+        }
+        return mList.size
+    }
+
+    override fun onBindViewHolder(holder: NestedHolder, position: Int) {
+        holder.mTvClazz.text = mList[position]+" RecyclerView $position"
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): NestedHolder {
+        var view = View.inflate(mContext, R.layout.fragment_mine_item, null)
+        return NestedHolder(view)
+    }
+
+    class NestedHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var mTvClazz = itemView.findViewById<TextView>(R.id.tv_clazz)
+    }
+}
--- app/src/main/java/com/lingxiao/thefirst/mine/nestedscroll/NestedScrollActivity.kt
+++ app/src/main/java/com/lingxiao/thefirst/mine/nestedscroll/NestedScrollActivity.kt
@@ -1,11 +1,15 @@
 package com.lingxiao.thefirst.mine.nestedscroll
 
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import com.lingxiao.thefirst.R
 import com.lingxiao.thefirst.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_nested_scrolling.*
 
 class NestedScrollActivity : BaseActivity() {
+
+    private var mList: MutableList<String> = mutableListOf()
 
     override fun getLayoutResource(): Int {
         return R.layout.activity_nested_scrolling
@@ -15,6 +19,29 @@ class NestedScrollActivity : BaseActivity() {
         setTitle("NestedScrollTest")
         // 注意此处必须取消RecyclerView的焦点，否则进入页面不在顶部
         recycler_view.isFocusable = false
-    }
 
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+        mList.add("NestedScrollTest")
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.addItemDecoration(DividerItemDecoration(mContext, DividerItemDecoration.VERTICAL))
+        recycler_view.adapter = NestedAdapter(this, mList)
+
+
+    }
 }
