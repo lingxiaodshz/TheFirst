c65b382392c29c3f2a249a40593cba7fcbf504c6
lingxiaodshz <lingxiaodshz@163.com>
2018-09-06 22:31:05 +0800

    属性动画xml部分

--- app/src/main/java/com/lingxiao/thefirst/mine/animation/propertyanimation/PropertyAnimationActivity.kt
+++ app/src/main/java/com/lingxiao/thefirst/mine/animation/propertyanimation/PropertyAnimationActivity.kt
@@ -1,8 +1,12 @@
 package com.lingxiao.thefirst.mine.animation.propertyanimation
 
+import android.animation.Animator
+import android.animation.AnimatorInflater
 import android.os.Bundle
+import android.view.View
 import com.lingxiao.thefirst.R
 import com.lingxiao.thefirst.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_property_animation.*
 
 class PropertyAnimationActivity : BaseActivity() {
     override fun getLayoutResource(): Int {
@@ -11,6 +15,17 @@ class PropertyAnimationActivity : BaseActivity() {
 
     override fun initView(savedInstanceState: Bundle?) {
         setTitle("PropertyAnimation")
+
+        tv_xml.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                getXmlAnimator().start()
+            }
+        })
     }
 
+    fun getXmlAnimator(): Animator {
+        var animatorSet = AnimatorInflater.loadAnimator(this, R.animator.property_animator);
+        animatorSet.setTarget(iv_dog)
+        return animatorSet
+    }
 }
