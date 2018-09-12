58f86bbea5ec4c998c35e5f75479ce7cb37abd88
lingxiaodshz <lingxiaodshz@163.com>
2018-09-12 22:13:04 +0800

    RevealAnimationActivity

---
+++ app/src/main/java/com/lingxiao/thefirst/mine/animation/revealanimation/RevealAnimationActivity.kt
@@ -0,0 +1,12 @@
+package com.lingxiao.thefirst.mine.animation.revealanimation
+
+class RevealAnimationActivity : BaseActivity() {
+
+    override fun getLayoutResource(): Int {
+        return R.layout.activity_reveal_animation
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+}
