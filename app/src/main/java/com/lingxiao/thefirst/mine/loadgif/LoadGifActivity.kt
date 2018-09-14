5f2c7f81506495d6e8e752f27f6ac5363811340a
lingxiaodshz <lingxiaodshz@163.com>
2018-09-14 15:33:49 +0800

    增加显示圆形和圆角图片的功能

--- app/src/main/java/com/lingxiao/thefirst/mine/loadgif/LoadGifActivity.kt
+++ app/src/main/java/com/lingxiao/thefirst/mine/loadgif/LoadGifActivity.kt
@@ -2,9 +2,13 @@ package com.lingxiao.thefirst.mine.loadgif
 
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.lingxiao.thefirst.R
 import com.lingxiao.thefirst.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_load_gif.*
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+
 
 class LoadGifActivity : BaseActivity() {
     override fun getLayoutResource(): Int {
@@ -12,8 +16,15 @@ class LoadGifActivity : BaseActivity() {
     }
 
     override fun initView(savedInstanceState: Bundle?) {
+        val roundedCorners = RoundedCorners(15)
+        val options = RequestOptions.bitmapTransform(roundedCorners)
+
         Glide.with(this)
                 .load(R.drawable.gif_drawable)
+                //显示圆形图片
+//                .apply(RequestOptions.circleCropTransform())
+                // 显示带圆角的图片
+                .apply(options)
                 .into(iv_gif)
     }
 
